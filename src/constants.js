78234fdbb19d77445f9ca9b8385cbcfea51cdb21
areebqazi <66074608+areebqazi@users.noreply.github.com>
2024-08-04 17:04:57 +0530

    sdf

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 // const API = 'http://localhost:8080/api';
-const API = 'http://35.182.240.208:8080/api'
+// const API = 'http://35.182.240.208:8080/api'
+const API = 'http://api.edusoulconsultancy.com/api'
+
 // const API = 'https://safesport-backend.onrender.com/api'
 
 export {API}
